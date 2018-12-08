27cc3dcde5d06cdc49b5c270933769351e8f5d73
Artemka076 <artemkamasatka@mail.ru>
2018-12-08 08:07:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -239,7 +239,9 @@ bot.on('message', async message => {
             let nicknametest = message.member.displayName.toLowerCase();
             nicknametest = nicknametest.replace(/ /g, '');
             if (nicknametest.includes("[" + manytags[i].replace(/ /g, '').toLowerCase()) || nicknametest.includes(manytags[i].replace(/ /g, '').toLowerCase() + "]") || nicknametest.includes("(" + manytags[i].replace(/ /g, '').toLowerCase()) || nicknametest.includes(manytags[i].replace(/ /g, '').toLowerCase() + ")") || nicknametest.includes("{" + manytags[i].replace(/ /g, '').toLowerCase()) || nicknametest.includes(manytags[i].replace(/ /g, '').toLowerCase() + "}")){
-                if (!nicknametest.includes('ballas') && i != 1){
+                if (nicknametest.includes('ballas') && i == 1){
+                    
+                    }else{
                 let rolename = tags[manytags[i].toUpperCase()] // Указать название роли по соответствию с тэгом
                 let role = message.guild.roles.find(r => r.name == rolename); // Найти эту роль на discord сервере.
                 let reqchat = message.guild.channels.find(c => c.name == `requests-for-roles`); // Найти чат на сервере.
